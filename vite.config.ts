5ed7d86889200bfdde05112739787f89fa922896
Ivan Erricis <ivanerricis@libero.it>
2026-03-08 19:36:29 +0100

    feat: clean up vite.config.ts by removing redundant import and organizing configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,28 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import { cloudflare } from "@cloudflare/vite-plugin"
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), cloudflare()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    devSourcemap: true,
+    postcss: {
+      plugins: [
+      ],
+    },
+  },
+  optimizeDeps: {
+    exclude: ['flag-icons'],
+  },
+  build: {
+    rollupOptions: {
+      external: [/flag-icons/],
     },
   },
 })
